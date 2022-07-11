9b62d0c741cdcb7c8158090c36710552ea23edd9
Dennis Kamau <devjam.ke@gmail.com>
2022-07-11 20:00:17 +0300

    Create Login Form and Registration

---
+++ app/models/user.rb
@@ -0,0 +1,4 @@
+class User < ApplicationRecord
+    has_secure_password;
+    validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }, on: :create
+end
--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,8 @@ Rails.application.routes.draw do
   get 'pages/blog'
   get 'pages/contact'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+ get "sign_up", to: "registrations#new"
+ post "sign_up", to: "registrations#create"
   # Defines the root path route ("/")
   root "pages#index"
 end
